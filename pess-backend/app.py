cb614130c64e9c31332e62706a2568e5a2e11456
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-16 23:51:20 +0300

    Update app.py

--- pess-backend/app.py
+++ pess-backend/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from routes.auth import auth_bp
+from routes.admin import admin_bp
+from routes.user import user_bp
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
+
+# Register blueprints
+app.register_blueprint(auth_bp)
+app.register_blueprint(admin_bp)
+app.register_blueprint(user_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
