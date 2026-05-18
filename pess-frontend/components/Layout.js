957abd65edcd5a095767729fba710f768795a71b
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-18 23:05:33 +0300

    Create Layout.js

---
+++ pess-frontend/components/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className="container py-4">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
