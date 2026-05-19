cc162200a38a22fef93dedec8057de8a4344aeae
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-19 21:59:47 +0300

    Update user.py

--- pess-backend/routes/user.py
+++ pess-backend/routes/user.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, session, redirect, url_for, render_template
+from flask import Blueprint, render_template, session, flash, redirect, url_for
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
 @user_bp.route("/dashboard")
 def dashboard():
-    if session.get("role") == "user":
-        return render_template("user.html")
-    return redirect(url_for("auth.login"))
+    if session.get("role") != "user":
+        flash("Unauthorized access!", "danger")
+        return redirect(url_for("auth.login"))
+
+    return render_template("user_dashboard.html")
