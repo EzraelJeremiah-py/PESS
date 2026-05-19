4b7a7c570d8f39e4f1220477e3ec8e38d0d8e656
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-19 09:35:57 +0300

    Delete seed.py

--- seed.py
+++
@@ -1,5 +0,0 @@
-INSERT OR IGNORE INTO admins (username, password)
-VALUES ('admin', 'Admin@123#');
-
-INSERT OR IGNORE INTO users (serial, password) VALUES ('S4882.0001.2020', 'pass123');
-INSERT OR IGNORE INTO users (serial, password) VALUES ('S4882.0002.2020', 'mypassword');
