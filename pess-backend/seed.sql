9ffc48d184b6ec873ff5eac4a109ef4212d937fe
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-19 10:58:52 +0300

    Update seed.sql

--- pess-backend/seed.sql
+++ pess-backend/seed.sql
@@ -1,1 +1,8 @@
+-- Admins table seed
 INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
+
+-- Users table seed
+INSERT INTO users (serial, password) VALUES ('S4882.0001.2020', 'userpass1');
+INSERT INTO users (serial, password) VALUES ('S4882.0002.2020', 'userpass2');
+INSERT INTO users (serial, password) VALUES ('S4882.0003.2020', 'userpass3');
+
