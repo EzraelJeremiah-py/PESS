0fee807888d15d70efb6089e8470dd40c297d6dd
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-19 09:55:01 +0300

    Delete routes/__init__.py

--- routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth import auth_bp
-from .admin import admin_bp
-from .user import user_bp
-
-# This makes it easy to import all blueprints at once
-blueprints = [auth_bp, admin_bp, user_bp]
-
