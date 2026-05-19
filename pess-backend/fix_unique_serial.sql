015e604849ce76d5d81d90d16b4e66f5e72346cb
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-19 23:10:29 +0300

    Create fix_unique_serial.sql

---
+++ pess-backend/fix_unique_serial.sql
@@ -0,0 +1,5 @@
+-- Enforce uniqueness on users.serial
+CREATE UNIQUE INDEX IF NOT EXISTS idx_users_serial ON users(serial);
+
+-- Enforce uniqueness on admins.username
+CREATE UNIQUE INDEX IF NOT EXISTS idx_admins_username ON admins(username);
