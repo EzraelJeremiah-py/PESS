dff585a61b7765e76b4bb5c81321eae162e1cbe0
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-05-20 12:20:45 +0300

    Add fees blueprint to route initializations

--- pess-backend/routes/__init__.py
+++ pess-backend/routes/__init__.py
@@ -1,6 +1,7 @@
 from .auth import auth_bp
 from .admin import admin_bp
 from .user import user_bp
+from routes.fees import fees_bp
 
 # This makes it easy to import all blueprints at once
-blueprints = [auth_bp, admin_bp, user_bp]
+blueprints = [auth_bp, admin_bp, user_bp, fees_bp]
