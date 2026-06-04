509130234ed4b3a40391368e9f24cd07a6ad2309
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-04 22:06:54 +0300

    Update __init__.py

--- pess-backend/routes/__init__.py
+++ pess-backend/routes/__init__.py
@@ -5,8 +5,18 @@ from routes.fees import fees_bp
 from routes.meeting import meeting_bp
 from routes.late import late_bp
 from routes.suspensions import suspensions_bp
+from routes.parental import parental_bp
 
 
 
 # This makes it easy to import all blueprints at once
-blueprints = [auth_bp, admin_bp, user_bp, fees_bp, meeting_bp, late_bp, suspensions_bp]
+blueprints = [
+  auth_bp, 
+  admin_bp,
+  user_bp, 
+  fees_bp, 
+  meeting_bp, 
+  late_bp, 
+  suspensions_bp,
+  parental_bp
+]
