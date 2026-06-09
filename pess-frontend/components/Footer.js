2afb2932e9a5ac5c270f0f49f1c6e5032b0e15c4
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-09 21:39:54 +0300

    Delete pess-frontend/components directory

--- pess-frontend/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white text-center py-3 mt-5">
-      <p>PESS © {new Date().getFullYear()} | All rights reserved</p>
-    </footer>
-  );
-}
