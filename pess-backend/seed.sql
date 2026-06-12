f4eac123c658772c7b02b303fd80c031b682b628
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-12 11:34:09 +0300

    Seed admins and additional student records
    Added seed data for admins and additional students.

--- pess-backend/seed.sql
+++ pess-backend/seed.sql
@@ -1,9 +1,18 @@
+-- Admins table seed
+INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
+
+
 -- Students
 INSERT INTO users (username, serial, password, role)
 VALUES ('S4882.0001.2020', 'S4882.0001.2020', 'userpass1', 'student');
 
 INSERT INTO users (username, serial, password, role)
 VALUES ('S4882.0002.2020', 'S4882.0002.2020', 'userpass2', 'student');
+
+
+INSERT INTO users (username, serial, password, role)
+VALUES ('S4882.0003.2020', 'S4882.0003.2020', 'userpass3', 'student');
+
 
 -- Teachers
 INSERT INTO users (username, serial, password, role)
