fe952bb4ec215d7201a904c1f477d692e8285451
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-12 11:04:29 +0300

    Update user seed data for students and remove admin

--- pess-backend/seed.sql
+++ pess-backend/seed.sql
@@ -1,15 +1,15 @@
 -- Admins table seed
 INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
 
--- Users table seed
-INSERT INTO users (serial, password) VALUES ('S4882.0001.2020', 'userpass1');
-INSERT INTO users (serial, password) VALUES ('S4882.0002.2020', 'userpass2');
-INSERT INTO users (serial, password) VALUES ('S4882.0003.2020', 'userpass3');
+-- Users table seed (students)
+INSERT INTO users (username, serial, password, role) 
+VALUES ('John Doe', 'S4882.0001.2020', 'userpass1', 'student');
 
+INSERT INTO users (username, serial, password, role) 
+VALUES ('Jane Smith', 'S4882.0002.2020', 'userpass2', 'student');
 
--- Admins table seed
-INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
-
+INSERT INTO users (username, serial, password, role) 
+VALUES ('Michael Brown', 'S4882.0003.2020', 'userpass3', 'student');
 
 -- Teachers table seed
 INSERT INTO users (username, serial, password, role) 
@@ -23,4 +23,3 @@ VALUES ('Peter Smith', 'T003', 'Teacher@123#', 'teacher');
 
 INSERT INTO users (username, serial, password, role) 
 VALUES ('Grace Daniel', 'T004', 'Teacher@123#', 'teacher');
-
