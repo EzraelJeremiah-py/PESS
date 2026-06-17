04fba7b8abbd8951492960516aad61f74ca84527
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-17 15:33:18 +0300

    Update seed.sql

--- pess-backend/seed.sql
+++ pess-backend/seed.sql
@@ -4,6 +4,10 @@ INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
 -- Students
 INSERT INTO users (serial, password, role, class_stream) 
 VALUES ('S4882.0001.2020', 'userpass1', 'student', 'Form1A');
+INSERT INTO users (serial, password, role, class_stream) 
+VALUES ('S4882.0003.2020', 'userpass3', 'student', 'Form1A');
+INSERT INTO users (serial, password, role, class_stream) 
+VALUES ('S4882.0004.2020', 'userpass4', 'student', 'Form1A');
 
 INSERT INTO users (serial, password, role, class_stream) 
 VALUES ('S4882.0002.2020', 'userpass2', 'student', 'Form1B');
