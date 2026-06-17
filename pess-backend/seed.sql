e89cf7b206f8b4ad6283982fa3db5c1d35809347
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-06-17 16:44:59 +0300

    Update seed.sql

--- pess-backend/seed.sql
+++ pess-backend/seed.sql
@@ -7,68 +7,68 @@ INSERT INTO admins (username, password) VALUES ('admin', 'admin123');
 
 -- Form1A
 INSERT INTO users (serial, password, role, class_stream) VALUES
-('S4882.0001.2020', 'userpass1#', 'student', 'Form1A'),
-('S4882.0002.2020', 'userpass2#', 'student', 'Form1A'),
-('S4882.0003.2020', 'userpass3#', 'student', 'Form1A'),
-('S4882.0004.2020', 'userpass4#', 'student', 'Form1A'),
-('S4882.0005.2020', 'userpass5#', 'student', 'Form1A'),
-('S4882.0006.2020', 'userpass6#', 'student', 'Form1A'),
-('S4882.0007.2020', 'userpass7#', 'student', 'Form1A'),
-('S4882.0008.2020', 'userpass8#', 'student', 'Form1A'),
-('S4882.0009.2020', 'userpass9#', 'student', 'Form1A'),
-('S4882.0010.2020', 'userpass10#', 'student', 'Form1A');
+('S4882F1A001', 'userpass1', 'student', 'Form1A'),
+('S4882F1A002', 'userpass2', 'student', 'Form1A'),
+('S4882F1A003', 'userpass3', 'student', 'Form1A'),
+('S4882F1A004', 'userpass4', 'student', 'Form1A'),
+('S4882F1A005', 'userpass5', 'student', 'Form1A'),
+('S4882F1A006', 'userpass6', 'student', 'Form1A'),
+('S4882F1A007', 'userpass7', 'student', 'Form1A'),
+('S4882F1A008', 'userpass8', 'student', 'Form1A'),
+('S4882F1A009', 'userpass9', 'student', 'Form1A'),
+('S4882F1A010', 'userpass10', 'student', 'Form1A');
 
 -- Form1B
 INSERT INTO users (serial, password, role, class_stream) VALUES
-('S4882.0011.2020', 'userpass11#', 'student', 'Form1B'),
-('S4882.0012.2020', 'userpass12#', 'student', 'Form1B'),
-('S4882.0013.2020', 'userpass13#', 'student', 'Form1B'),
-('S4882.0014.2020', 'userpass14#', 'student', 'Form1B'),
-('S4882.0015.2020', 'userpass15#', 'student', 'Form1B'),
-('S4882.0016.2020', 'userpass16#', 'student', 'Form1B'),
-('S4882.0017.2020', 'userpass17#', 'student', 'Form1B'),
-('S4882.0018.2020', 'userpass18#', 'student', 'Form1B'),
-('S4882.0019.2020', 'userpass19#', 'student', 'Form1B'),
-('S4882.0020.2020', 'userpass20#', 'student', 'Form1B');
+('S4882F1B001', 'userpass11', 'student', 'Form1B'),
+('S4882F1B002', 'userpass12', 'student', 'Form1B'),
+('S4882F1B003', 'userpass13', 'student', 'Form1B'),
+('S4882F1B004', 'userpass14', 'student', 'Form1B'),
+('S4882F1B005', 'userpass15', 'student', 'Form1B'),
+('S4882F1B006', 'userpass16', 'student', 'Form1B'),
+('S4882F1B007', 'userpass17', 'student', 'Form1B'),
+('S4882F1B008', 'userpass18', 'student', 'Form1B'),
+('S4882F1B009', 'userpass19', 'student', 'Form1B'),
+('S4882F1B010', 'userpass20', 'student', 'Form1B');
 
 -- Form1C
 INSERT INTO users (serial, password, role, class_stream) VALUES
-('S4882.0021.2020', 'userpass21#', 'student', 'Form1C'),
-('S4882.0022.2020', 'userpass22#', 'student', 'Form1C'),
-('S4882.0023.2020', 'userpass23#', 'student', 'Form1C'),
-('S4882.0024.2020', 'userpass24#', 'student', 'Form1C'),
-('S4882.0025.2020', 'userpass25#', 'student', 'Form1C'),
-('S4882.0026.2020', 'userpass26#', 'student', 'Form1C'),
-('S4882.0027.2020', 'userpass27#', 'student', 'Form1C'),
-('S4882.0028.2020', 'userpass28#', 'student', 'Form1C'),
-('S4882.0029.2020', 'userpass29#', 'student', 'Form1C'),
-('S4882.0030.2020', 'userpass30#', 'student', 'Form1C');
+('S4882F1C001', 'userpass21', 'student', 'Form1C'),
+('S4882F1C002', 'userpass22', 'student', 'Form1C'),
+('S4882F1C003', 'userpass23', 'student', 'Form1C'),
+('S4882F1C004', 'userpass24', 'student', 'Form1C'),
+('S4882F1C005', 'userpass25', 'student', 'Form1C'),
+('S4882F1C006', 'userpass26', 'student', 'Form1C'),
+('S4882F1C007', 'userpass27', 'student', 'Form1C'),
+('S4882F1C008', 'userpass28', 'student', 'Form1C'),
+('S4882F1C009', 'userpass29', 'student', 'Form1C'),
+('S4882F1C010', 'userpass30', 'student', 'Form1C');
 
 -- Form1D
 INSERT INTO users (serial, password, role, class_stream) VALUES
-('S4882.0031.2020', 'userpass31#', 'student', 'Form1D'),
-('S4882.0032.2020', 'userpass32#', 'student', 'Form1D'),
-('S4882.0033.2020', 'userpass33#', 'student', 'Form1D'),
-('S4882.0034.2020', 'userpass34#', 'student', 'Form1D'),
-('S4882.0035.2020', 'userpass35#', 'student', 'Form1D'),
-('S4882.0036.2020', 'userpass36#', 'student', 'Form1D'),
-('S4882.0037.2020', 'userpass37#', 'student', 'Form1D'),
-('S4882.0038.2020', 'userpass38#', 'student', 'Form1D'),
-('S4882.0039.2020', 'userpass39#', 'student', 'Form1D'),
-('S4882.0040.2020', 'userpass40#', 'student', 'Form1D');
+('S4882F1D001', 'userpass31', 'student', 'Form1D'),
+('S4882F1D002', 'userpass32', 'student', 'Form1D'),
+('S4882F1D003', 'userpass33', 'student', 'Form1D'),
+('S4882F1D004', 'userpass34', 'student', 'Form1D'),
+('S4882F1D005', 'userpass35', 'student', 'Form1D'),
+('S4882F1D006', 'userpass36', 'student', 'Form1D'),
+('S4882F1D007', 'userpass37', 'student', 'Form1D'),
+('S4882F1D008', 'userpass38', 'student', 'Form1D'),
+('S4882F1D009', 'userpass39', 'student', 'Form1D'),
+('S4882F1D010', 'userpass40', 'student', 'Form1D');
 
 -- Form1E
 INSERT INTO users (serial, password, role, class_stream) VALUES
-('S4882.0041.2020', 'userpass41#', 'student', 'Form1E'),
-('S4882.0042.2020', 'userpass42#', 'student', 'Form1E'),
-('S4882.0043.2020', 'userpass43#', 'student', 'Form1E'),
-('S4882.0044.2020', 'userpass44#', 'student', 'Form1E'),
-('S4882.0045.2020', 'userpass45#', 'student', 'Form1E'),
-('S4882.0046.2020', 'userpass46#', 'student', 'Form1E'),
-('S4882.0047.2020', 'userpass47#', 'student', 'Form1E'),
-('S4882.0048.2020', 'userpass48#', 'student', 'Form1E'),
-('S4882.0049.2020', 'userpass49#', 'student', 'Form1E'),
-('S4882.0050.2020', 'userpass50#', 'student', 'Form1E');
+('S4882F1E001', 'userpass41', 'student', 'Form1E'),
+('S4882F1E002', 'userpass42', 'student', 'Form1E'),
+('S4882F1E003', 'userpass43', 'student', 'Form1E'),
+('S4882F1E004', 'userpass44', 'student', 'Form1E'),
+('S4882F1E005', 'userpass45', 'student', 'Form1E'),
+('S4882F1E006', 'userpass46', 'student', 'Form1E'),
+('S4882F1E007', 'userpass47', 'student', 'Form1E'),
+('S4882F1E008', 'userpass48', 'student', 'Form1E'),
+('S4882F1E009', 'userpass49', 'student', 'Form1E'),
+('S4882F1E010', 'userpass50', 'student', 'Form1E');
 
 -- =========================
 -- Teachers: 1 per stream
