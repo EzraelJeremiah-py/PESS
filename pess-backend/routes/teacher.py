a19967deca7a2e75e7aa271536ab01fc8669b6d4
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-07-23 23:12:07 +0300

    Change after_request decorator to teacher_bp

--- pess-backend/routes/teacher.py
+++ pess-backend/routes/teacher.py
@@ -6,7 +6,7 @@ teacher_bp = Blueprint("teacher", __name__, url_prefix="/teacher")
 DB_PATH = os.path.join(os.path.dirname(__file__), "..", "pess.db")
 
 # Prevent cached pages after logout
-@admin_bp.after_request
+@teacher_bp.after_request
 def add_header(response):
     response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private"
     response.headers["Pragma"] = "no-cache"
