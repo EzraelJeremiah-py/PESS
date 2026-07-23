7dd884b3ed81d2a89a0f29198dff4b338b775552
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-07-23 23:02:00 +0300

    Prevent cached pages after user logout
    Add cache control headers to prevent caching after logout.

--- pess-backend/routes/user.py
+++ pess-backend/routes/user.py
@@ -4,6 +4,14 @@ import sqlite3, os
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 DB_PATH = os.path.join(os.path.dirname(__file__), "..", "pess.db")
 
+# Prevent cached pages after logout
+@admin_bp.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 @user_bp.route("/dashboard")
 def dashboard():
     if session.get("role") != "student":
