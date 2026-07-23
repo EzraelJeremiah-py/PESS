296372e5e1b44ef7835cb732f1699d2754f901a2
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-07-23 23:27:14 +0300

    Update chat.py

--- pess-backend/routes/chat.py
+++ pess-backend/routes/chat.py
@@ -5,6 +5,14 @@ from datetime import datetime
 chat_bp = Blueprint("chat", __name__, url_prefix="/chat")
 DB_PATH = os.path.join(os.path.dirname(__file__), "..", "pess.db")
 
+@chat_bp.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
+
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
